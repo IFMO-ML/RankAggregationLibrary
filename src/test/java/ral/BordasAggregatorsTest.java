bb4c58a86f0e1432eddbdfc33c9f0bea8c37ef93
Igor Tanfilyev <tanfilyev@gmail.com>
2015-05-17 18:05:47 +0300

    add gens test cases

--- src/test/java/ral/BordasAggregatorsTest.java
+++ src/test/java/ral/BordasAggregatorsTest.java
@@ -25,7 +25,22 @@ public class BordasAggregatorsTest {
     private static final Integer[] longListsBrodaGEM = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 56, 30, 57, 58, 46, 59, 47, 60, 41, 48, 42};
     private static final Integer[] longListsBrodaL2N = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 46, 47, 56, 57, 48, 58, 59, 60, 49, 41};
 
-    private void rankVerification(RankAggregator<Integer> rankAggregator, ListOfRanks<Integer> ranks, Integer[] expected) {
+    private static final ListOfRanks<String> gens = new ListOfRanks<>(new String[][]{
+            {"HPN", "AMACR", "CYP1B1", "ATF5", "BRCA1", "LGALS3", "MYC", "PCDHGC3", "WT1", "TFF3", "MARCKS", "OS-9", "CCND2", "NME1", "DRRK1A", "TRAP1", "FMO5", "ZHX2", "RPL36AL", "ITPR3", "GCSH", "DDB2", "TFCP2", "TRAM1", "YTHDF3"},
+            {"HPN", "AMACR", "OACT2", "GDF15", "FASN", "ANK3", "KRT18", "UAP1", "GRP58", "PPIB", "KRT7", "NME1", "STRA13", "DAPK1", "TMEM4", "CANX", "TRA1", "PRSS8", "EMTPD6", "PPP1CA", "ACADSB", "PTPLB", "TMEM23", "MRPL3", "SLC19A1"},
+            {"OGT", "AMACR", "FASN", "HPN", "UAP1", "GUCY1A3", "OACT2", "SLC19A1", "KRT18", "EEF2", "STRA13", "ALCAM", "GDF15", "NME1", "CALR", "SND1", "STAT6", "TCEB3", "EIF4A1", "LMAN1", "MAOA", "ATP6V0B", "PPIB", "FMO5", "SLC7A5"},
+            {"AMACR", "HPN", "NME2", "CBX3", "GDF15", "MTHFD2", "MRPL3", "SLC25A6", "NME1", "COX6C", "JTV1", "CCNG2", "AP3S1", "EEF2", "RAN", "PRKACA", "RAD23B", "PSAP", "CCT2", "G3BP", "EPRS", "CKAP1", "LIG3", "SNX4", "NSMAF"},
+            {"HPN", "SLC25A6", "EEF2", "SAT", "NME2", "LDHA", "CANX", "NACA", "FASN", "SND1", "KRT18", "RPL15", "TNFSF10", "SERP1", "GRP58", "ALCAM", "GDF15", "TMEM4", "CCT2", "SLC39A6", "RPL5", "RPS13", "MTHFD2", "G3BP2", "UAP1"},
+    });
+
+    private static final String[] gensBrodaARM = new String[]{"HPN", "AMACR", "GDF15", "FASN", "NME1", "KRT18", "EEF2", "NME2", "OACT2", "SLC25A6", "UAP1", "CANX", "GRP58", "STRA13", "SND1", "OGT", "ALCAM", "CYP1B1", "MTHFD2", "ATF5", "CBX3", "SAT", "BRCA1", "MRPL3", "LGALS3"};
+    private static final String[] gensBrodaMED = new String[]{"HPN", "AMACR", "FASN", "KRT18", "GDF15", "NME1", "EEF2", "UAP1", "CYP1B1", "ATF5", "BRCA1", "LGALS3", "MYC", "PCDHGC3", "WT1", "TFF3", "MARCKS", "OS-9", "CCND2", "DYRK1A", "TRAP1", "FMO5", "ZHX2", "RPL36AL", "ITPR3"};
+    private static final String[] gensBrodaGEM = new String[]{"HPN", "AMACR", "FASN", "GDF15", "NME2", "SLC25A6", "EEF2", "OACT2", "OGT", "KRT18", "NME1", "UAP1", "CYP1B1", "ATF5", "CBX3", "SAT", "CANX", "BRCA1", "GRP58", "MTHFD2", "STRA13", "LGALS3", "ANK3", "GUCY1A3", "LDHA"};
+    private static final String[] gensBrodaL2N = new String[]{"HPN", "AMACR", "GDF15", "NME1", "FASN", "KRT18", "EEF2", "NME2", "UAP1", "OACT2", "SLC25A6", "STRA13", "CANX", "GRP58", "SND1", "ALCAM", "TMEM4", "MTHFD2", "MRPL3", "PPIB", "OGT", "CYP1B1", "SLC19A1", "ATF5", "CBX3"};
+//    private static final String[] gensBrodaARM = new String[]{"HPN", "AMACR", "FASN", "GDF15", "UAP1", "OACT2", "KRT18", "SLC25A6", "NME1", "EEF2", "STRA13", "NME2", "CANX", "SND1", "GRP58", "ALCAM", "PPIB", "TMEM4", "CYP1B1", "MTHFD2", "ATF5", "MRPL3", "BRCA1", "LGALS3", "MYC"};
+//    private static final String[] gensBrodaARM = new String[]{"HPN", "AMACR", "FASN", "GDF15", "NME2", "UAP1", "OACT2", "SLC25A6", "KRT18", "EEF2", "STRA13", "NME1", "CANX", "ALCAM", "GRP58", "SND1", "FMO5", "TMEM4", "CCT2", "PRKACA", "MTHFD2", "PTPLB", "PPIB", "MRPL3", "SLC19A1"};
+
+    private <T> void rankVerification(RankAggregator<T> rankAggregator, ListOfRanks<T> ranks, T[] expected) {
         Assert.assertEquals(Arrays.toString(expected), Arrays.toString(Arrays.copyOf(rankAggregator.aggregate(ranks).toArray(), expected.length)));
     }
 
@@ -40,6 +55,11 @@ public class BordasAggregatorsTest {
     }
 
     @Test
+    public void arithmeticAverageRankerTestGens() {
+        rankVerification(BordasAggregators.arithmeticAverageRanker(), gens, gensBrodaARM);
+    }
+
+    @Test
     public void medianRankerTestIceCreamFlavors() {
         rankVerification(BordasAggregators.medianRanker(), iceCreamFlavors, iceCreamFlavorsAnswer);
     }
@@ -47,6 +67,11 @@ public class BordasAggregatorsTest {
     @Test
     public void medianRankerTestLongLists() {
         rankVerification(BordasAggregators.medianRanker(), longLists, longListsBrodaMED);
+    }
+
+    @Test
+    public void medianRankerTestGens() {
+        rankVerification(BordasAggregators.medianRanker(), gens, gensBrodaMED);
     }
 
     @Test
@@ -60,6 +85,11 @@ public class BordasAggregatorsTest {
     }
 
     @Test
+    public void geometricMeanRankerTestGens() {
+        rankVerification(BordasAggregators.geometricMeanRanker(), gens, gensBrodaGEM);
+    }
+
+    @Test
     public void L2NormRankerTestIceCreamFlavors() {
         rankVerification(BordasAggregators.L2NormRanker(), iceCreamFlavors, iceCreamFlavorsAnswer);
     }
@@ -67,5 +97,10 @@ public class BordasAggregatorsTest {
     @Test
     public void L2NormRankerTestLongLists() {
         rankVerification(BordasAggregators.L2NormRanker(), longLists, longListsBrodaL2N);
+    }
+
+    @Test
+    public void L2NormRankerTestGens() {
+        rankVerification(BordasAggregators.L2NormRanker(), gens, gensBrodaL2N);
     }
 }
--- src/test/java/ral/MCTest.java
+++ src/test/java/ral/MCTest.java
@@ -26,7 +26,20 @@ public class MCTest {
     private static final Integer[] longListsMC2Answer2 = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 46, 47, 48, 49, 50, 57, 56, 58, 59, 60};
     private static final Integer[] longListsMC3Answer = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 46, 47, 48, 49, 50, 56, 57, 58, 59, 60};
 
-    private void rankVerification(RankAggregator<Integer> rankAggregator, ListOfRanks<Integer> ranks, Integer[] expected) {
+
+    private static final ListOfRanks<String> gens = new ListOfRanks<>(new String[][]{
+            {"HPN", "AMACR", "CYP1B1", "ATF5", "BRCA1", "LGALS3", "MYC", "PCDHGC3", "WT1", "TFF3", "MARCKS", "OS-9", "CCND2", "NME1", "DRRK1A", "TRAP1", "FMO5", "ZHX2", "RPL36AL", "ITPR3", "GCSH", "DDB2", "TFCP2", "TRAM1", "YTHDF3"},
+            {"HPN", "AMACR", "OACT2", "GDF15", "FASN", "ANK3", "KRT18", "UAP1", "GRP58", "PPIB", "KRT7", "NME1", "STRA13", "DAPK1", "TMEM4", "CANX", "TRA1", "PRSS8", "EMTPD6", "PPP1CA", "ACADSB", "PTPLB", "TMEM23", "MRPL3", "SLC19A1"},
+            {"OGT", "AMACR", "FASN", "HPN", "UAP1", "GUCY1A3", "OACT2", "SLC19A1", "KRT18", "EEF2", "STRA13", "ALCAM", "GDF15", "NME1", "CALR", "SND1", "STAT6", "TCEB3", "EIF4A1", "LMAN1", "MAOA", "ATP6V0B", "PPIB", "FMO5", "SLC7A5"},
+            {"AMACR", "HPN", "NME2", "CBX3", "GDF15", "MTHFD2", "MRPL3", "SLC25A6", "NME1", "COX6C", "JTV1", "CCNG2", "AP3S1", "EEF2", "RAN", "PRKACA", "RAD23B", "PSAP", "CCT2", "G3BP", "EPRS", "CKAP1", "LIG3", "SNX4", "NSMAF"},
+            {"HPN", "SLC25A6", "EEF2", "SAT", "NME2", "LDHA", "CANX", "NACA", "FASN", "SND1", "KRT18", "RPL15", "TNFSF10", "SERP1", "GRP58", "ALCAM", "GDF15", "TMEM4", "CCT2", "SLC39A6", "RPL5", "RPS13", "MTHFD2", "G3BP2", "UAP1"},
+    });
+
+    private static final String[] gensMC1Answer = new String[]{"HPN", "AMACR", "GDF15", "NME1", "FASN", "EEF2", "KRT18", "NME2", "SLC25A6", "UAP1", "OACT2", "CYP1B1", "ATF5", "CANX", "OGT", "BRCA1", "MTHFD2", "LGALS3", "GRP58", "SND1", "CBX3", "MRPL3", "MYC", "STRA13", "ALCAM"};
+    private static final String[] gensMC2Answer = new String[]{"HPN", "AMACR", "GDF15", "FASN", "KRT18", "NME1", "EEF2", "UAP1", "OACT2", "NME2", "SLC25A6", "OGT", "SAT", "NACA", "LDHA", "CANX", "SLC19A1", "ANK3", "ALCAM", "GUCY1A3", "SND1", "MTHFD2", "TMEM4", "MRPL3", "GRP58"};
+    private static final String[] gensMC3Answer = new String[]{"HPN", "AMACR", "GDF15", "NME1", "FASN", "EEF2", "KRT18", "UAP1", "NME2", "SLC25A6", "OACT2", "CANX", "GRP58", "STRA13", "SND1", "ALCAM", "MTHFD2", "MRPL3", "SLC19A1", "TMEM4", "PPIB", "CCT2", "FMO5", "OGT", "CYP1B1"};
+
+    private <T> void rankVerification(RankAggregator<T> rankAggregator, ListOfRanks<T> ranks, T[] expected) {
         Assert.assertEquals(Arrays.toString(expected), Arrays.toString(Arrays.copyOf(rankAggregator.aggregate(ranks).toArray(), expected.length)));
     }
 
@@ -41,6 +54,11 @@ public class MCTest {
     }
 
     @Test
+    public void markovChainTestMC1gens() {
+        rankVerification(MarkovChainAggregators.MC1(), gens, gensMC1Answer);
+    }
+
+    @Test
     public void markovChainTestMC2IceCreamFlavors() {
         rankVerification(MarkovChainAggregators.MC2(), iceCreamFlavors, iceCreamFlavorsAnswer);
     }
@@ -51,6 +69,11 @@ public class MCTest {
     }
 
     @Test
+    public void markovChainTestMC2gens() {
+        rankVerification(MarkovChainAggregators.MC2(), gens, gensMC2Answer);
+    }
+
+    @Test
     public void markovChainTestMC3IceCreamFlavors() {
         rankVerification(MarkovChainAggregators.MC3(), iceCreamFlavors, iceCreamFlavorsAnswer);
     }
@@ -58,5 +81,10 @@ public class MCTest {
     @Test
     public void markovChainTestMC3LongList() {
         rankVerification(MarkovChainAggregators.MC3(), longLists, longListsMC3Answer);
+    }
+
+    @Test
+    public void markovChainTestMC3gens() {
+        rankVerification(MarkovChainAggregators.MC3(), gens, gensMC3Answer);
     }
 }
